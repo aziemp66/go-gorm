7711a077a9eb8738f5171f20fd0d7ec9126eb259
Azie Melza Pratama <aziemp66@gmail.com>
2023-06-09 01:07:50 +0700

    init gorm

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+
+	DB "github.com/aziemp66/go-gorm/pkg/postgres"
+)
 
 func main() {
 	router := gin.Default()
+
+	db := DB.NewDB()
+
+	log.Println(db)
 
 	err := router.Run(":5000")
 	if err != nil {
---
+++ pkg/postgres/init.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	dsn := "host=localhost user=aziemp66 password=azie122333 dbname=go_gorm port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Database Connected")
+
+	return db
+}
